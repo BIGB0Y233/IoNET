3d0213604c10fd1ca46cf4c6e2fc8d75f4d5c9bf
mxh <73646723+theshyxmu@users.noreply.github.com>
2022-01-23 21:30:09 +0800

    Update dataset.py

--- IONET/dataset.py
+++ IONET/dataset.py
@@ -18,24 +18,24 @@ def load_oxiod_dataset(imu_data_filename, gt_data_filename):
     acc_data = imu_data[:, 10:13]#加速度a的数据，取二维数组中的第10到12列的数据
     
     pos_data = gt_data[:, 2:5]＃位置position的数据，取二维数组中的第2列到第4列的数据
-    ori_data = np.concatenate([gt_data[:, 8:9], gt_data[:, 5:8]], axis=1)#将第8列的数据和第5到7列的数据进行行拼接axis=1为行为0为列拼接
+    ori_data = np.concatenate([gt_data[:, 8:9], gt_data[:, 5:8]], axis=1)#方向orientation将第8列的数据和第5到7列的数据进行行拼接axis=1为行为0为列拼接
 
     print("load_oxiod")
     print(pos_data.shape,gyro_data.shape[0])
-    return gyro_data, acc_data, pos_data, ori_data#返回重力，加速度，位置，原始数据的数据
+    return gyro_data, acc_data, pos_data, ori_data#返回重力，加速度，位置，方向的数据
 
 
 def load_dataset_6d_quat(gyro_data, acc_data, pos_data, ori_data, window_size=200, stride=10):
     #gyro_acc_data = np.concatenate([gyro_data, acc_data], axis=1)
 
     init_p = pos_data[window_size//2 - stride//2, :]#py中／／表示向下取整，窗大小，和步长大小
-    init_q = ori_data[window_size//2 - stride//2, :]＃初始数据
+    init_q = ori_data[window_size//2 - stride//2, :]＃方向数据
 
     #x = []
     x_gyro = []＃重力
     x_acc = []＃加速度
     y_delta_p = []＃变化的位置
-    y_delta_q = []＃变化的
+    y_delta_q = []＃变化的方向
 
     print(gyro_data.shape[0],pos_data.shape)
     for idx in range(0, gyro_data.shape[0] - window_size - 1, stride):#遍历整个数据，shape[0]表示垂直大小
@@ -46,15 +46,15 @@ def load_dataset_6d_quat(gyro_data, acc_data, pos_data, ori_data, window_size=20
         p_a = pos_data[idx + window_size//2 - stride//2, :]#位置a
         p_b = pos_data[idx + window_size//2 + stride//2, :]#位置b
 
-        q_a = quaternion.from_float_array(ori_data[idx + window_size//2 - stride//2, :])#四元数法位置a
-        q_b = quaternion.from_float_array(ori_data[idx + window_size//2 + stride//2, :])
+        q_a = quaternion.from_float_array(ori_data[idx + window_size//2 - stride//2, :])#方向a
+        q_b = quaternion.from_float_array(ori_data[idx + window_size//2 + stride//2, :])#方向b
 
         delta_p = np.matmul(quaternion.as_rotation_matrix(q_a).T, (p_b.T - p_a.T)).T#矩阵相乘，变化的位置p
 
         delta_q = force_quaternion_uniqueness(q_a.conjugate() * q_b)#共轭后转化为四元数
 
-        y_delta_p.append(delta_p)
-        y_delta_q.append(quaternion.as_float_array(delta_q))
+        y_delta_p.append(delta_p)#变化的位置数据集
+        y_delta_q.append(quaternion.as_float_array(delta_q))#变化的方向集
 
 
     #x = np.reshape(x, (len(x), x[0].shape[0], x[0].shape[1]))#转化格式
